34397b1e97c12acacc33247201d54d64bea04d34
Kevin Fang <kfang1233@gmail.com>
2018-12-08 18:31:42 -0500

    Added skills

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Typist from 'react-typist'
 import Grid from '@material-ui/core/Grid'
@@ -8,6 +7,7 @@ import 'bulma/css/bulma.css'
 
 let projects_list = require('./projects.json')
 let experience_list = require('./experience.json')
+let skills = require('./skills.json')
 
 projects_list.sort((a, b) => {
   return a.priority - b.priority
@@ -23,6 +23,7 @@ class App extends Component {
           </Typist>
         </div>
         <img
+          alt="headshot"
           style={{width: '40vh', marginTop: 10}}
           src={'./headshot.png'}
         />
@@ -33,19 +34,19 @@ class App extends Component {
           justify="center">
           <div>
             <a href="https://github.com/kevin-fang/">
-              <img src={'./github.png'} style={{width: '4vh', margin: 5, opacity: 0.5}}/>
+              <img alt="GitHub" src={'./github.png'} style={{width: '4vh', margin: 5, opacity: 0.5}}/>
             </a>
             <a href="https://linkedin.com/in/kevin-fang/">
-              <img src={'./linkedin.png'} style={{width: '4vh', margin: 5, opacity: 0.5}}/>
+              <img alt="LinkedIn" src={'./linkedin.png'} style={{width: '4vh', margin: 5, opacity: 0.5}}/>
             </a>
             <a href="mailto:kevinzfang@gmail.com">
-              <img src={'./mail.png'} style={{width: '4vh', margin: 5, marginTop: 10, opacity: 0.5}}/>
+              <img alt="Email" src={'./mail.png'} style={{width: '4vh', margin: 5, marginTop: 10, opacity: 0.5}}/>
             </a>
           </div>
         </Grid>
         <div style={{fontSize: '3vh', marginTop: '2vh'}}>
           I'm currently a freshman studying Computer Science at NYU,<br/>
-          interested in Machine Learning/AI and financial markets.<br/>
+          interested in Machine Learning/AI, bioinformatics, and financial markets.<br/>
           <div style={{paddingTop: '1.5vh'}}>Currently, I'm exploring software engineering and data science.</div>
         </div>
       </div>
@@ -56,9 +57,9 @@ class App extends Component {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div style={{position: 'absolute', right: 0, margin: 10}} id="navbarBasicExample" className="navbar-menu">
-          <a className="navbar-item" href="#about">
+          {/*<a className="navbar-item" href="#about">
             About
-          </a>
+          </a>*/}
           <a className="navbar-item" href="#projects">
             Portfolio
           </a>
@@ -90,8 +91,8 @@ class App extends Component {
           </div>
         </div>
         <footer className="card-footer">
-          <a href={project.link} target="_blank" className="card-footer-item">Link</a>
-          <a href={project.github} target="_blank" className="card-footer-item">GitHub</a>
+          <a href={project.link} target="_blank" rel="noopener noreferrer" className="card-footer-item">Link</a>
+          <a href={project.github} target="_blank" rel="noopener noreferrer" className="card-footer-item">GitHub</a>
         </footer>
       </div>
     )
@@ -116,14 +117,15 @@ class App extends Component {
 
   getExperience = () => {
     return (
-      <div style={{textAlign: 'left', margin: 10}}>
+      <div style={{textAlign: 'left', margin: 25}}>
         {
           experience_list.map((experience) => {
-            return <div>
-              <b style={{fontSize: 40}}>{experience.company_name}</b><br/>
+            return <div style={{marginTop: "2.5vh"}}>
+              <i>{experience.dates}</i><br/>
+              <b style={{fontSize: "3vh", marginTop: 0}}>{experience.position} @ <a target="_blank" rel="noopener noreferrer" href={experience.company_link}>{experience.company_name}</a></b><br/>
               {experience.description.map((description) => {
-                return <li style={{marginLeft: 10}}>{description}</li>
-              })}<br/>
+                return <div style={{marginLeft: 25}}>> {description}</div>
+              })}
             </div>
           })
         }
@@ -135,6 +137,85 @@ class App extends Component {
     return (
       <div>
         [Contact goes here]
+      </div>
+    )
+  }
+
+  getSkills = () => {
+    return (
+      <div>
+        <span style={{fontSize: '2.5vh'}}>Languages</span><br/><br/>
+        <div className="SideBySide">
+          {
+            skills.languages.map((skill) => {
+              if (skill.icon) {
+                return (<div style={{marginLeft: 20, marginRight: 20}}>
+                  <i className={skill.icon} style={{fontSize: "5vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>)
+              } else {
+                return <div style={{marginLeft: 20, marginRight: 20}}>
+                  <img src={skill.logo} style={{width: "4.65vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>
+              }
+            })
+          }
+        </div><br/>
+        <span style={{fontSize: '2.5vh'}}>Libraries</span><br/><br/>
+        <div className="SideBySide">
+          {
+            skills.libraries.map((skill) => {
+              if (skill.icon) {
+                return (<div style={{marginLeft: 20, marginRight: 20}}>
+                  <i className={skill.icon} style={{fontSize: "5vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>)
+              } else {
+                return <div style={{marginLeft: 20, marginRight: 20}}>
+                  <img src={skill.logo} style={{width: "4.65vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>
+              }
+            })
+          }
+        </div><br/>
+        <span style={{fontSize: '2.5vh'}}>Web</span><br/><br/>
+        <div className="SideBySide">
+          {
+            skills.web.map((skill) => {
+              if (skill.icon) {
+                return (<div style={{marginLeft: 20, marginRight: 20}}>
+                  <i className={skill.icon} style={{fontSize: "5vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>)
+              } else {
+                return <div style={{marginLeft: 20, marginRight: 20}}>
+                  <img src={skill.logo} style={{width: "4.65vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>
+              }
+            })
+          }
+        </div><br/>
+        <span style={{fontSize: '2.5vh'}}>Miscellaneous</span><br/><br/>
+        <div className="SideBySide">
+          {
+            skills.misc.map((skill) => {
+              if (skill.icon) {
+                return (<div style={{marginLeft: 20, marginRight: 20}}>
+                  <i className={skill.icon} style={{fontSize: "5vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>)
+              } else {
+                return <div style={{marginLeft: 20, marginRight: 20}}>
+                  <img src={skill.logo} style={{width: "4.65vh", opacity: 0.6}}/><br/>
+                  <span style={{fontSize: '1.5vh'}}>{skill.name}</span>
+                </div>
+              }
+            })
+          }
+        </div><br/>
       </div>
     )
   }
@@ -151,19 +232,23 @@ class App extends Component {
         </a>
         <div id="projects" style={{minHeight: '100vh', width: "100%"}}>
           <div style={{fontSize: '10vh', margin: 30, marginTop: 0, marginBottom: 0}}>
-            Portfolio
+            <b>Portfolio</b>
           </div>
           {this.getProjects()}
         </div>
         <div id="experience" style={{minHeight: '100vh', width: "100%"}}>
           <div style={{fontSize: '10vh', margin: 30, marginTop: 0}}>
-            Experience
+            <b>Experience</b>
           </div>
           {this.getExperience()}
+          <div style={{fontSize: '7vh', marginTop: 0}}>
+            <b>Skills</b>
+          </div>
+          {this.getSkills()}
         </div>
         <div id="contact" style={{minHeight: '100vh', width: "100%"}}>
           <div style={{fontSize: '10vh', margin: 30, marginTop: 0}}>
-            Contact
+            <b>Contact</b>
           </div>
           {this.getContact()}
         </div>
