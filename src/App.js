dfe00c2f9a451c65c0255e673f9e61c76c832ebe
Kevin Fang <kfang1233@gmail.com>
2019-03-09 22:30:39 -0500

    Fixed spacing

--- src/App.js
+++ src/App.js
@@ -160,7 +160,7 @@ class App extends Component {
             <br />
             <br />I love technology and learning new things. Currently, I'm
             exploring software engineering and data science. For the past few
-            years, I've been working with
+            years, I've been working with{' '}
             <a href="https://curoverse.com">Curoverse Research</a> to help us
             understand the human genome better.
           </span>
