fc56753b8befb5c62ffd81d35e5119a131e6cace
kfang <kfang1233@gmail.com>
2020-03-27 14:27:14 -0400

    Updates

---
+++ precache-manifest.3bd6f216288fb98947a71e511bd59072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ddb790e277cb03fbfafd",
+    "url": "/static/js/main.910eeff3.chunk.js"
+  },
+  {
+    "revision": "5a0425109e52be609699",
+    "url": "/static/js/2.2c38c37e.chunk.js"
+  },
+  {
+    "revision": "ddb790e277cb03fbfafd",
+    "url": "/static/css/main.07d946e5.chunk.css"
+  },
+  {
+    "revision": "5a0425109e52be609699",
+    "url": "/static/css/2.b45d41d7.chunk.css"
+  },
+  {
+    "revision": "b9c0ac55636fb30ba43b495c187eba8b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44f779ca5ac85d3307a07026ef2fff0c.js"
+  "/precache-manifest.3bd6f216288fb98947a71e511bd59072.js"
 );
 
 workbox.clientsClaim();
