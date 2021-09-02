ba331523069fe2b2579cc6f5ce492e63ed097992
Kevin Fang <kfang1233@gmail.com>
2021-09-01 22:37:23 -0400

    Updates

---
+++ precache-manifest.7b1b8cb89f3e858baf6cedd97fa083b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f367137c2d953fa82b0",
+    "url": "/static/css/main.a97b8a15.chunk.css"
+  },
+  {
+    "revision": "3f367137c2d953fa82b0",
+    "url": "/static/js/main.dba2644f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53c443d2b5d8180b8788",
+    "url": "/static/css/2.6e11d83a.chunk.css"
+  },
+  {
+    "revision": "53c443d2b5d8180b8788",
+    "url": "/static/js/2.7c676fc5.chunk.js"
+  },
+  {
+    "revision": "1d4afc7a162db52ffb5a705296a6b1bf",
+    "url": "/index.html"
+  }
+];
